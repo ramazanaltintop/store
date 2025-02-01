7adc0e3de78477caf178eadda60a890a97f23f5f
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-02-01 13:14:34 +0300

    CartSummaryViewComponent created

---
+++ Store/StoreApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            _cart = cartService;
+        }
+        public string Invoke()
+        {
+            return _cart.Lines.Count().ToString();
+        }
+    }
+}
