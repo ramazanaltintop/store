30f2afa2fac572231f6098d486c62297522aeca3
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-02-01 19:48:55 +0300

    context updated

--- Store/DataAccess/Concrete/EntityFramework/Contexts/ProductDbContext.cs
+++ Store/DataAccess/Concrete/EntityFramework/Contexts/ProductDbContext.cs
@@ -9,6 +9,7 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public ProductDbContext()
         {
 
