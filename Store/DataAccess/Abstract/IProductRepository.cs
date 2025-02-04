fdb16116fc82bf02d17ed4fd7b5447ba9d32c47b
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-02-04 12:41:28 +0300

    IProductRepository.cs updated

--- Store/DataAccess/Abstract/IProductRepository.cs
+++ Store/DataAccess/Abstract/IProductRepository.cs
@@ -1,11 +1,13 @@
 using DataAccess.Base;
 using Entities.Concrete;
+using Entities.RequestParameters;
 
 namespace DataAccess.Abstract
 {
     public interface IProductRepository : IRepositoryBase<Product>
     {
         IQueryable<Product> GetAllProducts(bool trackChanges);
+        IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters parameters);
         IQueryable<Product> GetShowcaseProducts(bool trackChanges);
         Product? GetOneProduct(int id, bool trackChanges);
         void CreateOneProduct(Product product);
