18770cb748dade4db34623fd50fd2692b8dce433
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-02-04 12:42:17 +0300

    IProductService.cs updated

--- Store/Business/Abstract/IProductService.cs
+++ Store/Business/Abstract/IProductService.cs
@@ -1,11 +1,13 @@
 using Entities.Concrete;
 using Entities.Dtos;
+using Entities.RequestParameters;
 
 namespace Business.Abstract
 {
     public interface IProductService
     {
         IEnumerable<Product> GetAllProducts(bool trackChanges);
+        IEnumerable<Product> GetAllProductsWithDetails(ProductRequestParameters parameters);
         IEnumerable<Product> GetShowcaseProducts(bool trackChanges);
         Product? GetOneProduct(int id, bool trackChanges);
         void CreateOneProduct(ProductDtoForInsertion productDto);
