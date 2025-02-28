08a8e06d30e70dd85dd8d10fd10d7661ebc86b4a
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-03-01 01:12:17 +0300

    Roles completed

--- Store/Business/Abstract/IAuthService.cs
+++ Store/Business/Abstract/IAuthService.cs
@@ -5,18 +5,16 @@ namespace Business.Abstract
 {
     public interface IAuthService
     {
+        Task<IdentityResult> CreateRole(RoleDtoForInsertion roleDto);
         IEnumerable<IdentityRole> Roles { get; }
         Task<IdentityRole> GetOneRole(string id);
         Task<RoleDtoForUpdate> GetOneRoleForUpdate(string id);
-        Task<IdentityResult> CreateRole(RoleDtoForInsertion roleDto);
-        Task<IdentityResult> DeleteOneRole(string id);
         Task Update(RoleDtoForUpdate roleDto);
-
-
+        Task<IdentityResult> DeleteOneRole(string id);
+        Task<IdentityResult> CreateUser(UserDtoForCreation userDto);
         IEnumerable<IdentityUser> GetAllUsers();
         Task<IdentityUser> GetOneUser(string userName);
         Task<UserDtoForUpdate> GetOneUserForUpdate(string userName);
-        Task<IdentityResult> CreateUser(UserDtoForCreation userDto);
         Task Update(UserDtoForUpdate userDto);
     }
 }
